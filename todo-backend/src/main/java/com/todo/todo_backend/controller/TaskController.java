cb58b2fc95f6be4aafb243df70671c2cd92a4799
LahiruMadushann <lahirumadushandl@gmail.com>
2025-10-04 15:23:21 +0530

    feat : implement controller for task creation

---
+++ todo-backend/src/main/java/com/todo/todo_backend/controller/TaskController.java
@@ -0,0 +1,26 @@
+package com.todo.todo_backend.controller;
+
+import com.todo.todo_backend.dto.TaskRequest;
+import com.todo.todo_backend.dto.TaskResponse;
+import com.todo.todo_backend.service.TaskService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+    private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> createTask(@Valid @RequestBody TaskRequest request) {
+        TaskResponse response = taskService.createTask(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
