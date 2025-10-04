99e6dce081c527fb0543b05367f9027fbd458697
LahiruMadushann <lahirumadushandl@gmail.com>
2025-10-04 15:04:36 +0530

    feat : implement task request dto

---
+++ todo-backend/src/main/java/com/todo/todo_backend/dto/TaskRequest.java
@@ -0,0 +1,12 @@
+package com.todo.todo_backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record TaskRequest(
+        @NotBlank(message = "Title is required")
+        @Size(max = 200, message = "Title must be less than 200 characters")
+        String title,
+        String description
+) {
+}
