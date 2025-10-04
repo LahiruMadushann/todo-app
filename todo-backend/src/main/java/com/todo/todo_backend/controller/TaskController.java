9e8fcbe970471c3b6e7826f8905953ad5e5b72c1
LahiruMadushann <lahirumadushandl@gmail.com>
2025-10-04 15:32:58 +0530

    feat : add versioning for task api

--- todo-backend/src/main/java/com/todo/todo_backend/controller/TaskController.java
+++ todo-backend/src/main/java/com/todo/todo_backend/controller/TaskController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/api/v1/tasks")
 @RequiredArgsConstructor
 public class TaskController {
     private final TaskService taskService;
