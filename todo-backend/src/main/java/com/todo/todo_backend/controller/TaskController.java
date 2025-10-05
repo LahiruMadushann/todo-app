fdac08a8780a5b86757b915f8f63088366b689c2
LahiruMadushann <lahirumadushandl@gmail.com>
2025-10-05 08:24:57 +0530

    feat : implement controller for get tasks

--- todo-backend/src/main/java/com/todo/todo_backend/controller/TaskController.java
+++ todo-backend/src/main/java/com/todo/todo_backend/controller/TaskController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -22,5 +21,11 @@ public class TaskController {
     public ResponseEntity<TaskResponse> createTask(@Valid @RequestBody TaskRequest request) {
         TaskResponse response = taskService.createTask(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponse>> getRecentTasks() {
+        List<TaskResponse> tasks = taskService.getRecentTasks();
+        return ResponseEntity.ok(tasks);
     }
 }
